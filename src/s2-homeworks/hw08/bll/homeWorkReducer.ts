52cad582b900a10f0dad8c3622dcb2f131208844
kollo <t.a.abdulmanov@gmail.com>
2023-03-08 02:10:42 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,23 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const state_res = state;
+            state_res.sort((a: UserType, b: UserType) => {
+                if (action.payload === "up") {
+                    if (a.name > b.name) {
+                        return 1;
+                    } else return -1;
+                } else if (action.payload === "down"){
+                    if (a.name < b.name) {
+                        return 1
+                    } else return -1;
+                } else return state;
+                })
+            return state_res // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            const state_filtrated = state.filter((x: UserType) => x.age >= action.payload)
+            return state_filtrated // need to fix
         }
         default:
             return state
