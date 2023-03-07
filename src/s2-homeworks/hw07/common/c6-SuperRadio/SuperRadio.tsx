6efea38c624c1313240a9b709988d85f49309d6c
kollo <t.a.abdulmanov@gmail.com>
2023-03-07 18:29:15 +0300

    hw7 2th step

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -51,7 +51,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
-                        name={o.value}
+                        name={"inputs"}
                         value={o.id}
                       checked={o.id === value}
                       onChange={onChangeCallback}
