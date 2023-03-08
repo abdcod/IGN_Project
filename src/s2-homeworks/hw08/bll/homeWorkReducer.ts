ee169d189cdce54dfabefc4a7fdb56a040473894
kollo <t.a.abdulmanov@gmail.com>
2023-03-08 03:15:04 +0300

    HW8 3rd step

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,11 +4,11 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-            const state_res = state;
-            state_res.sort((a: UserType, b: UserType) => {
+        case 'sort': // by name
+            const state_res = [...state];
+            state_res.sort((a: UserType, b: UserType) : number => {
                 if (action.payload === "up") {
                     if (a.name > b.name) {
                         return 1;
@@ -17,14 +17,12 @@ export const homeWorkReducer = (state: any, action: any): any => { // need to fi
                     if (a.name < b.name) {
                         return 1
                     } else return -1;
-                } else return state;
+                } else return 0;
                 })
             return state_res // need to fix
-        }
-        case 'check': {
-            const state_filtrated = state.filter((x: UserType) => x.age >= action.payload)
-            return state_filtrated // need to fix
-        }
+        case 'check':
+            const state_filtrated = state.filter((x: UserType) => x.age >= action.payload);
+            return state_filtrated; // need to fix
         default:
             return state
     }
