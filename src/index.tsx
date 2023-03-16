cc5cf3001d1615cb88a0ba9ec2c6e1cb8cf9bd0b
kollo <t.a.abdulmanov@gmail.com>
2023-03-16 04:12:46 +0300

    HW9 added

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,14 @@ import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        {/*для дз 10*/}
         <Provider store={store}>
             <App/>
         </Provider>
-    </React.StrictMode>
 )
+
+// <React.StrictMode>
+// </React.StrictMode>
+// {/*для дз 10*/}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
